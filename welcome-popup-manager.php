1a4f09855ab632da70a70a37e0c7bc740189e9a3
Fede Cardoso <hernan.f.cardoso@gmail.com>
2026-01-17 20:56:45 -0300

    Initial plugin structure and main file

---
+++ welcome-popup-manager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Welcome Popup Manager
+ * Plugin URI:  https://github.com/TU_USUARIO/welcome-popup-manager
+ * Description: Displays a configurable welcome popup with image, description and link.
+ * Version:     1.0.0
+ * Author:      Tu Nombre
+ * Author URI:  https://www.linkedin.com/in/cardosohernan/
+ * Text Domain: welcome-popup-manager
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
